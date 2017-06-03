ccf88aa4bdf44789a7aea145b19e740044069786
Hristo Spasov <hristo_sp99@abv.bg>
2017-06-03 21:34:39 +0300

    Use react rendering

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+// Database
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host : 'localhost',
+  database: 'cinemaDB',
+  user: 'cinemaAdmin',
+  password: 'cinema_Pass123'
+});
+db.connect();
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
---
+++ cinemadb_queries.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE cinemaDB CHARACTER SET utf8;
+
+USE cinemaDB;
+
+CREATE TABLE Users (
+	Username VARCHAR(15) NOT NULL PRIMARY KEY,
+    Password VARCHAR(60) NOT NULL,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
+    Age INTEGER NOT NULL
+);
+
+CREATE TABLE Movies (
+	Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    AgeRestriction INTEGER NOT NULL,
+    FirstProjection DATETIME NOT NULL,
+    LastProjection DATETIME NOT NULL,
+    Length INTEGER NOT NULL
+);
+
+CREATE TABLE Projections (
+	Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    MovieId INTEGER NOT NULL,
+    FOREIGN KEY(MovieId) REFERENCES Movies(Id)
+);
+
+CREATE TABLE ProjectionViewers (
+	Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ProjectionId INTEGER NOT NULL,
+    Username INTEGER NOT NULL,
+    FOREIGN KEY(ProjectionId) REFERENCES Projections(Id),
+    FOREIGN KEY(Username) REFERENCES Users(Username)
+);
+
+CREATE TABLE Halls (
+	Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Seats INTEGER NOT NULL
+);
+
+ALTER TABLE Users ADD Role VARCHAR(20) NOT NULL;
+
+INSERT INTO Users 
+	(Username, Password, FirstName, LastName, Age, Role)
+VALUES
+	( 'test', 'test', 'Testing', 'Tester', 20, 'user' ),
+    ( 'test2', 'test2', 'Another', 'Tester', 20, 'user' );
+    
+INSERT INTO Movies 
+	(Title, AgeRestriction, FirstProjection, LastProjection, Length)
+VALUES
+	( 'Pirates of the Carribean', 18, '2017-04-04 08:30:00', '2017-05-20 19:20:00', 120 ),
+	( 'The Dictator', 12, '2015-09-04 12:30:00', '2015-12-28 21:45:00', 93 );
