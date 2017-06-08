33302fef09655aa7863403531f6c52fbc8d32a1d
Hristo Spasov <hristo_sp99@abv.bg>
2017-06-08 22:15:44 +0300

    Implement sample getting from database

--- app.js
+++ app.js
@@ -8,13 +8,106 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var books = require('./routes/books');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Database
+var db = mysql.createConnection({
+  host : 'localhost',
+  user: 'cinemaAdmin',
+  password: 'cinema_Pass123'
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  db.query("DROP DATABASE IF EXISTS cinemaDB;", function (err, result) {
+    if (err) throw err;
+    console.log("No database");
+  });
+
+  db.query("CREATE DATABASE cinemaDB CHARACTER SET utf8;", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+
+  db.query("USE cinemaDB;", function (err, result) {
+    if (err) throw err;
+    console.log("Using cinemaDB");
+  });
+
+  var createTableUsers = "CREATE TABLE Users ( \
+	  Username VARCHAR(15) NOT NULL PRIMARY KEY,\
+    Password VARCHAR(60) NOT NULL,\
+    FirstName VARCHAR(30) NOT NULL,\
+    LastName VARCHAR(30) NOT NULL,\
+    Age INTEGER NOT NULL,\
+    Role VARCHAR(20) NOT NULL\
+    );";
+  db.query(createTableUsers, function (err, result) {
+    if (err) throw err;
+    console.log("Created Users table");
+  });
+
+  var createTableMovies = "CREATE TABLE Movies (\
+	  Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,\
+    Title VARCHAR(100) NOT NULL,\
+    AgeRestriction INTEGER NOT NULL,\
+    FirstProjection DATETIME NOT NULL,\
+    LastProjection DATETIME NOT NULL,\
+    Length INTEGER NOT NULL\
+    );";
+  db.query(createTableMovies, function (err, result) {
+    if (err) throw err;
+    console.log("Created Movies table");
+  });
+
+  var createTableProjections = "CREATE TABLE Projections (\
+	  Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,\
+    MovieId INTEGER NOT NULL,\
+    FOREIGN KEY(MovieId) REFERENCES Movies(Id)\
+    );";
+  db.query(createTableProjections, function (err, result) {
+    if (err) throw err;
+    console.log("Created Projections table");
+  });
+
+  var createTableProjectionViewers = "CREATE TABLE ProjectionViewers (\
+	  Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,\
+    ProjectionId INTEGER NOT NULL,\
+    Username VARCHAR(15) NOT NULL,\
+    FOREIGN KEY(ProjectionId) REFERENCES Projections(Id),\
+    FOREIGN KEY(Username) REFERENCES Users(Username)\
+    );";
+  db.query(createTableProjectionViewers, function (err, result) {
+    if (err) throw err;
+    console.log("Created ProjectionViewers table");
+  });
+
+  var createTableHalls = "CREATE TABLE Halls (\
+	  Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,\
+    Seats INTEGER NOT NULL\
+    );";
+  db.query(createTableHalls, function (err, result) {
+    if (err) throw err;
+    console.log("Created Halls table");
+  });
+
+  var insertUsers = "INSERT INTO Users \
+  	(Username, Password, FirstName, LastName, Age, Role)\
+    VALUES\
+	  ( 'test', 'test', 'Testing', 'Tester', 20, 'user' ),\
+    ( 'test2', 'test2', 'Another', 'Tester', 20, 'user' );";
+  db.query(insertUsers, function (err, result) {
+    if (err) throw err;
+    console.log("Inserted users");
+  });
+});
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
@@ -31,16 +124,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/books', books);
 
-// Database
-var db = mysql.createConnection({
-  host : 'localhost',
-  database: 'cinemaDB',
-  user: 'cinemaAdmin',
-  password: 'cinema_Pass123'
-});
-db.connect();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var db = req.db; 
+  db.query("SELECT * FROM Users;", function(err, results, fields) {
+    if (err) {
+        res.send("errordatabase");
+    } else{
+        res.render("user", { title: "List of All Users",
+                             users:results});
+    }
+  })
+
 });
 
 module.exports = router;
