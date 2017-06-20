97316ff42f9d91c117e1fda330a16221f8d2fa98
Rangel Ivanov <radito3@mail.bg>
2017-06-20 15:20:08 +0300

    Add profile redirection on edit and delete

--- routes/profile.js
+++ routes/profile.js
@@ -59,7 +59,7 @@ router.post('/settings', securityCheck.isLoggedIn, (req, res) => {
                             console.log(err);
                             res.status(500).send('Internal Server Error');
                         } else {
-                            res.status(201).send('Successfully changed your profile settings!');
+                            res.status(201).redirect('/profile');
                         }
                     }
                 );
@@ -82,7 +82,7 @@ router.get('/delete', securityCheck.isLoggedIn, (req, res) => {
                 console.log(err);
                 res.status(500).send('Internal Server Error');
             }
-            res.status(200).send('Successfully deleted profile');
+            res.status(200).redirect('/');
         }
     );
 });
