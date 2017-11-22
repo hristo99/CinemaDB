2b7d3a8c78f83014131c6db275f226e773e51fac
Hristo Spasov <hristo_sp99@abv.bg>
2017-11-22 10:02:59 +0200

    Add inert comments, seats and notification types database requests on db creation.

--- scripts/createDatabase.js
+++ scripts/createDatabase.js
@@ -482,46 +482,192 @@ connection.query(insertProjectionViewers, err => {
     console.log("Inserted projection viewers");
 });
 
-// const insertProjectionViewers = `INSERT INTO ProjectionViewers
-//     (ProjectionId, Username)
+const insertComments = `INSERT INTO Comments
+    (MovieId, UserId, Text, Date)
+VALUES
+    ( 1, 2, 'I havent watched the movie but its good :)', '2017-12-14 22:34:58'),
+    ( 1, 1, 'How is that a comedy?', '2017-12-14 22:58:09'),
+    ( 2, 3, 'My brain hurts', '2017-12-01 19:33:17'),
+    ( 3, 2, 'And they found Dory. Oops that was a spoiler.', '2017-11-30 14:00:44'),
+    ( 5, 5, 'Good for kids', '2018-12-25 01:17:08'),
+    ( 6, 6, 'They didnt mention China so its good. But they didnt build a wall with Mexico. So its bad', '2017-12-20 17:12:00');`;
+connection.query(insertComments, err => {
+    if (err) throw err;
+    console.log("Inserted comments");
+});
+
+const insertSeats = `INSERT INTO Seats
+    (HallId, Row, Position)
+VALUES
+    (1, 1, 1),
+    (1, 1, 2),
+    (1, 1, 3),
+    (1, 1, 4),
+    (1, 1, 5),
+    (1, 1, 6),
+    (1, 2, 1),
+    (1, 2, 2),
+    (1, 2, 3),
+    (1, 2, 4),
+    (1, 2, 5),
+    (1, 2, 6),
+    (1, 3, 1),
+    (1, 3, 2),
+    (1, 3, 3),
+    (1, 3, 4),
+    (1, 3, 5),
+    (1, 3, 6),
+    (2, 1, 1),
+    (2, 1, 2),
+    (2, 1, 3),
+    (2, 1, 4),
+    (2, 1, 5),
+    (2, 1, 6),
+    (2, 2, 1),
+    (2, 2, 2),
+    (2, 2, 3),
+    (2, 2, 4),
+    (2, 2, 5),
+    (2, 2, 6),
+    (2, 3, 1),
+    (2, 3, 2),
+    (2, 3, 3),
+    (2, 3, 4),
+    (2, 3, 5),
+    (2, 3, 6),
+    (3, 1, 1),
+    (3, 1, 2),
+    (3, 1, 3),
+    (3, 1, 4),
+    (3, 1, 5),
+    (3, 1, 6),
+    (3, 2, 1),
+    (3, 2, 2),
+    (3, 2, 3),
+    (3, 2, 4),
+    (3, 2, 5),
+    (3, 2, 6),
+    (3, 3, 1),
+    (3, 3, 2),
+    (3, 3, 3),
+    (3, 3, 4),
+    (3, 3, 5),
+    (3, 3, 6),
+    (4, 1, 1),
+    (4, 1, 2),
+    (4, 1, 3),
+    (4, 1, 4),
+    (4, 1, 5),
+    (4, 1, 6),
+    (4, 2, 1),
+    (4, 2, 2),
+    (4, 2, 3),
+    (4, 2, 4),
+    (4, 2, 5),
+    (4, 2, 6),
+    (4, 3, 1),
+    (4, 3, 2),
+    (4, 3, 3),
+    (4, 3, 4),
+    (4, 3, 5),
+    (4, 3, 6),
+    (5, 1, 1),
+    (5, 1, 2),
+    (5, 1, 3),
+    (5, 1, 4),
+    (5, 1, 5),
+    (5, 1, 6),
+    (5, 2, 1),
+    (5, 2, 2),
+    (5, 2, 3),
+    (5, 2, 4),
+    (5, 2, 5),
+    (5, 2, 6),
+    (5, 3, 1),
+    (5, 3, 2),
+    (5, 3, 3),
+    (5, 3, 4),
+    (5, 3, 5),
+    (5, 3, 6),
+    (6, 1, 1),
+    (6, 1, 2),
+    (6, 1, 3),
+    (6, 1, 4),
+    (6, 1, 5),
+    (6, 1, 6),
+    (6, 2, 1),
+    (6, 2, 2),
+    (6, 2, 3),
+    (6, 2, 4),
+    (6, 2, 5),
+    (6, 2, 6),
+    (6, 3, 1),
+    (6, 3, 2),
+    (6, 3, 3),
+    (6, 3, 4),
+    (6, 3, 5),
+    (6, 3, 6),
+    (7, 1, 1),
+    (7, 1, 2),
+    (7, 1, 3),
+    (7, 1, 4),
+    (7, 1, 5),
+    (7, 1, 6),
+    (7, 2, 1),
+    (7, 2, 2),
+    (7, 2, 3),
+    (7, 2, 4),
+    (7, 2, 5),
+    (7, 2, 6),
+    (7, 3, 1),
+    (7, 3, 2),
+    (7, 3, 3),
+    (7, 3, 4),
+    (7, 3, 5),
+    (7, 3, 6),
+    (8, 1, 1),
+    (8, 1, 2),
+    (8, 1, 3),
+    (8, 1, 4),
+    (8, 1, 5),
+    (8, 1, 6),
+    (8, 2, 1),
+    (8, 2, 2),
+    (8, 2, 3),
+    (8, 2, 4),
+    (8, 2, 5),
+    (8, 2, 6),
+    (8, 3, 1),
+    (8, 3, 2),
+    (8, 3, 3),
+    (8, 3, 4),
+    (8, 3, 5),
+    (8, 3, 6);`;
+connection.query(insertSeats, err => {
+    if (err) throw err;
+    console.log("Inserted seats");
+});
+
+const insertNotificationTypes = `INSERT INTO NotificationTypes
+    (Subject)
+VALUES
+    ('friend request'),
+    ('projection reminder'),
+    ('viewers group request'),
+    ('viewers group info');`;
+connection.query(insertNotificationTypes, err => {
+    if (err) throw err;
+    console.log("Inserted notification types");
+});
+
+// const insertNotifications = `INSERT INTO Notifications
+//     (UserId, Information, TypeId, Date)
 // VALUES
-//     (46, 'test'),
-//     (47, 'test'),
-//     (48, 'test2'),
-//     (48, 'test'),
-//     (49, 'hspasov'),
-//     (49, 'radito3'),
-//     (49, 'santa'),
-//     (1, 'dont'),
-//     (2, 'hspasov'),
-//     (3, 'radito3'),
-//     (3, 'santa'),
-//     (5, 'test'),
-//     (5, 'test2'),
-//     (5, 'hspasov'),
-//     (5, 'radito3'),
-//     (5, 'dont'),
-//     (5, 'santa'),
-//     (10, 'test'),
-//     (10, 'test2'),
-//     (15, 'dont'),
-//     (15, 'santa'),
-//     (15, 'test'),
-//     (31, 'hspasov'),
-//     (33, 'hspasov'),
-//     (38, 'hspasov'),
-//     (18, 'hspasov'),
-//     (24, 'hspasov'),
-//     (18, 'radito3'),
-//     (45, 'radito3'),
-//     (40, 'radito3'),
-//     (26, 'radito3'),
-//     (31, 'radito3'),
-//     (19, 'radito3'),
-//     (20, 'radito3');`;
-// connection.query(insertProjectionViewers, err => {
+//     (1, '{"friendId": 3}', 1, '2017-11-13 18:00:00'),
+//     (1, '{"friendId": 5, ""}', 2, '2017-12-25 01:00:00');`;
+// connection.query(insertNotifications, err => {
 //     if (err) throw err;
-//     console.log("Inserted ProjectionViewers");
+//     console.log("Inserted notifications");
 // });
 
 connection.end();
