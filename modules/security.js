b3d21f706bb9713f7303208482aadc08c2f6323b
Rangel Ivanov <rivanov@localhost.localdomain>
2018-01-03 10:29:35 +0200

    Add admin checks in security.js

--- modules/security.js
+++ modules/security.js
@@ -1,7 +1,26 @@
 module.exports = {
     isLoggedIn(req, res, next) {
-        if (req.isAuthenticated())
+        if (req.isAuthenticated()) {
             return next();
+        }
         res.redirect('/login');
+    },
+    isCinemaAdmin(req, res, next) {
+        if (req.user && req.user.Role == 'cinemaAdmin') {
+            return next();
+        }
+        res.status(401).send('Not authorized');
+    },
+    isMovieAdmin(req, res, next) {
+        if (req.user && req.user.Role == 'movieAdmin') {
+            return next();
+        }
+        res.status(401).send('Not authorized');
+    },
+    isSystemAdmin(req, res, next) {
+        if (req.user && req.user.Role == 'systemAdmin') {
+            return next();
+        }
+        res.status(401).send('Not authorized');
     }
 }
